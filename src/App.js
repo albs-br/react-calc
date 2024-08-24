114a0b52fded4899326579c8cf1c2e60055611e9
ANDRE BAPTISTA <andrebaptista@petrobras.com.br>
2024-08-24 18:39:11 -0300

    v.0.9.0

--- src/App.js
+++ src/App.js
@@ -45,16 +45,11 @@ function App() {
   };
 
   const executeOperation = () => {
-    let temp;
-    if(calcProperties.operation === "+") {
-      temp = (parseInt(calcProperties.firstOperandValue) + parseInt(calcProperties.calcVisorValue)).toString();
-    }
-    else if(calcProperties.operation === "-") {
-      temp = (parseInt(calcProperties.firstOperandValue) - parseInt(calcProperties.calcVisorValue)).toString();
-    }
+
+    let result = doOperation();
 
     setCalcProperties({
-      calcVisorValue: temp,
+      calcVisorValue: result,
       firstOperandValue: calcProperties.calcVisorValue,
       operation: "",
       isResult: true,
@@ -64,21 +59,28 @@ function App() {
 
   const executeConcatOperation = (nextOperation) => {
 
-    let temp;
-    if(calcProperties.operation === "+") {
-      temp = (parseInt(calcProperties.firstOperandValue) + parseInt(calcProperties.calcVisorValue)).toString();
-    }
-    else if(calcProperties.operation === "-") {
-      temp = (parseInt(calcProperties.firstOperandValue) - parseInt(calcProperties.calcVisorValue)).toString();
-    }
+    let result = doOperation();
 
     setCalcProperties({ ...calcProperties, 
-      calcVisorValue: temp,
-      firstOperandValue: temp,
+      calcVisorValue: result,
+      firstOperandValue: result,
       operation: nextOperation,
       isResult: true 
     });
 
+  }
+
+  const doOperation = () => {
+    switch (calcProperties.operation) {
+      case "+":
+        return (parseInt(calcProperties.firstOperandValue) + parseInt(calcProperties.calcVisorValue)).toString();
+    
+      case "-":
+        return (parseInt(calcProperties.firstOperandValue) - parseInt(calcProperties.calcVisorValue)).toString();
+    
+      default:
+        break;
+    }
   }
 
   const buttonEqualPressed = (e) => {
@@ -114,11 +116,6 @@ function App() {
           <h2>React Calculator</h2>
         </div>
         <form className="w3-container w3-card-4">
-          <br />
-          {calcProperties.firstOperandValue}<br />
-          {calcProperties.operation}<br />
-          {calcProperties.calcVisorValue}<br />
-          {(calcProperties.isResult) ? "true" : "false" }<br />
           <p>
             <input className="calcVisor w3-input w3-border" type="text" required="" value={calcProperties.calcVisorValue} readOnly="readonly"></input>
           </p>
